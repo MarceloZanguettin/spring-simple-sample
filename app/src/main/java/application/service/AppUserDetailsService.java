6747635a1786d065ebc0f51c720fc086c14c50fc
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-13 21:53:41 +0000

    commit

---
+++ app/src/main/java/application/service/AppUserDetailsService.java
@@ -0,0 +1,13 @@
+package application.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    public UserDetails loadUserByUsername(String username) {
+
+    }
+}
